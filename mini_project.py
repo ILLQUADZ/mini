35c31438db0d3d9a5933f266b596a6e5a06553e0
ILLQUADZ <illquad4z@gmail.com>
2025-02-24 20:51:47 +0900

    Update mini_project.py

--- mini_project.py
+++ mini_project.py
@@ -1,11 +1,19 @@
-from selenium import webdriver as wb
-from selenium.webdriver.common.by import By
+
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
 import requests
 import os
+
+import selenium #셀레니움
+import pandas as pd #csv를 읽고 dataframe을 사용하기 위한 pandas
+from selenium import webdriver as wb #브라우저를 띄우고 컨트롤하기 위한 webdriver
+from selenium.webdriver.common.keys import Keys #브라우저에 키입력 용
+from selenium.webdriver.common.by import By #webdriver를 이용해 태그를 찾기 위함
+from selenium.webdriver.support.ui import WebDriverWait #Explicitly wait을 위함
+from webdriver_manager.chrome import ChromeDriverManager #크롬에서 크롤링 진행 크롬 웹 드라이버 설치시 필요
+from selenium.webdriver.support import expected_conditions as EC #브라우저에 특정 요소 상태 확인을 위해
+from bs4 import BeautifulSoup #브라우저 태그를 가져오고 파싱하기 위함
+from selenium.common.exceptions import NoSuchElementException,StaleElementReferenceException,TimeoutException #예외처리를 위한 예외들 
+import time
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
