cdee00c449bc5c2d8b4216f792eac86a11197ab5
ILLQUADZ <illquad4z@gmail.com>
2025-02-24 21:27:07 +0900

    Update mini_project.py

--- mini_project.py
+++ mini_project.py
@@ -29,7 +29,7 @@ driver.get(url)
 # Continue with the rest of your script...
 time.sleep(10)  # Increased wait time
 
-Advertisement = driver.find_elements(By.CLASS_NAME, "dPXjn")
+Advertisement = driver.find_elements(By.CSS_SELECTOR, '.dPXjn')
 data1 = driver.find_elements(By.CSS_SELECTOR, '.YwYLL')
 
 print(f"Advertisement elements found: {len(Advertisement)}")
