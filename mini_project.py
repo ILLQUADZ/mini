14a6895791b7729ae5154189b26dfb07ce222bdc
ILLQUADZ <illquad4z@gmail.com>
2025-02-24 12:21:46 +0900

    Update mini_project.py

--- mini_project.py
+++ mini_project.py
@@ -17,7 +17,7 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--remote-debugging-port=9222")  # 포트 지정
 start_point = "서울기술교육센터"
 url = f"https://map.naver.com/p/search/%EC%84%9C%EC%9A%B8%EA%B8%B0%EC%88%A0%EA%B5%90%EC%9C%A1%EC%84%BC%ED%84%B0?c=15.00,0,0,0,dh"
-driver = wb.Chrome('/urs/bin/chromedriver', chrome_options=options)
+driver = wb.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
 driver.get(url)
 time.sleep(1)
 driver.switch_to.default_content()
